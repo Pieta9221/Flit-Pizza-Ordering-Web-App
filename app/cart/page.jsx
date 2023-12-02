f56ffeeb51ab9e5bfa631df7b5c31bc3ace9e198
Pieta <mercy_nwaodu@yahoo.com>
2023-12-02 06:47:17 +0100

    fixed table scroll

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -76,16 +76,17 @@ function onApprove(data) {
   return (
     <div className="flex flex-col md:flex-row items-start mx-auto p-4 my-20 gap-4">
       <div className="md:w-2/3 w-full mb-4 md:mb-0">
-        <div className="overflow-auto">
-          <table className="w-full border-b border-gray-300 px-3">
+        <div className="overflow-auto px-5">
+          <table className="w-[100%] table-auto border-collapse border-b border-gray-300">
+          
           <thead>
               <tr>
-                <th className="border-b text-left">Product</th>
-                <th className="border-b text-left">Name</th>
-                <th className="border-b text-left">Extras</th>
-                <th className="border-b text-left">Price</th>
-                <th className="border-b text-left">Quantity</th>
-                <th className="border-b text-left">Total</th>
+                <th className="border-b text-left p-3">Product</th>
+                <th className="border-b text-left p-3">Name</th>
+                <th className="border-b text-left p-3">Extras</th>
+                <th className="border-b text-left p-3">Price</th>
+                <th className="border-b text-left p-3">Quantity</th>
+                <th className="border-b text-left p-3">Total</th>
               </tr>
             </thead>
             <tbody>
@@ -93,11 +94,11 @@ function onApprove(data) {
                 <td className="border-b align-top">
                   <Image src="/burga-pizza.png" alt="pizza-icon" width={100} height={100} />
                 </td>
-                <td className="border-b pt-2 align-top">Burga Pizza</td>
-                <td className="border-b pt-2 align-top"></td>
-                <td className="border-b pt-2 align-top">$40</td>
-                <td className="border-b pt-2 align-top">4</td>
-                <td className="border-b pt-2 align-top">$160</td>
+                <td className="border-b p-3 whitespace-nowrap align-top">Burga Pizza</td>
+                <td className="border-b p-3 whitespace-nowrap align-top"></td>
+                <td className="border-b p-3 whitespace-nowrap align-top">$40</td>
+                <td className="border-b p-3 whitespace-nowrap align-top">4</td>
+                <td className="border-b p-3 whitespace-nowrap align-top">$160</td>
               </tr>
               
             </tbody>
