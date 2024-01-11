9a1587e346ddfd4ad8a0178f0c82880250786e3c
Pieta <mercy_nwaodu@yahoo.com>
2024-01-11 07:38:06 +0100

    search page

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,13 +2,55 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineClose, AiOutlineSearch } from 'react-icons/ai'
+
+
+
+const SearchModal = ({ onClose }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    // Implement your search logic here, for now, just log the query
+    console.log('Search query:', searchQuery);
+    onClose();
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black">
+      <div className="p-8 rounded-md w-4/5 h-3/5 bg-transparent flex flex-col items-center relative">
+        <button onClick={onClose} className="text-white hover:text-[#e20202] absolute top-[-10%] right-2 focus:outline-none p-2 border border-white rounded-full">
+          <AiOutlineClose size={24} />
+        </button>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="What pizza are you craving?"
+          className="border p-4 w-full rounded-full text-lg bg-transparent placeholder-white focus:outline-none mt-4"
+        />
+        <div className="absolute top-[16%] md:top-[15%] right-10 cursor-pointer text-white">
+          <AiOutlineSearch size={24} onClick={handleSearch} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 const Navbar = () => {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false)
+  const [isSearchModalOpen, setIsSearchModalOpen] = useState(false)
 
   const toggleMobileNav = () => {
     setIsMobileNavOpen(!isMobileNavOpen)
+  }
+
+  const openSearchModal = () => {
+    setIsSearchModalOpen(true)
+  };
+
+  const closeSearchModal = () => {
+    setIsSearchModalOpen(false)
   };
 
   return (
@@ -50,9 +92,9 @@ const Navbar = () => {
           </li>
           </div>
           <div className="flex md:flex items-center justify-center space-x-8">
-            <Link href="#">
+            <div onClick={openSearchModal} className='cursor-pointer'>
             <Image src="/search-icon.png" alt="Search" width={24} height={24} className='cursor-pointer' />
-          </Link>
+          </div>
           <Link href="/cart">
             <div className="relative cursor-pointer">
               <Image src="/cart-blue.png" alt="Cart" width={30} height={30}/>
@@ -104,6 +146,10 @@ const Navbar = () => {
           
         </ul>
       )}
+
+      {/* Search Modal */}
+      {isSearchModalOpen && <SearchModal onClose={closeSearchModal} />}
+    
     </nav>
   )
 }
